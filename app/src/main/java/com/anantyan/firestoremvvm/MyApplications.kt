5f017319fa2280f8cdcd1fce048043014beff220
Arya Rezza Anantya <43742778+anantyan@users.noreply.github.com>
2022-08-11 23:47:26 +0700

    Update MyApplications.kt

--- app/src/main/java/com/anantyan/firestoremvvm/MyApplications.kt
+++ app/src/main/java/com/anantyan/firestoremvvm/MyApplications.kt
@@ -1,7 +1,18 @@
 package com.anantyan.firestoremvvm
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplications : Application()
+class MyApplications : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(applicationContext)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(PlayIntegrityAppCheckProviderFactory.getInstance())
+    }
+}
